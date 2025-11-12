3bbcaa38b0f4f5e47e3dc457159cc7fce2aa9b7f
Sourov-Ahmed26 <silenboyk7@gmail.com>
2025-11-12 22:04:58 +0600

    Ux improvments

--- src/Pages/MyTransaction/MyTransactionCard/TransactionCard.jsx
+++ src/Pages/MyTransaction/MyTransactionCard/TransactionCard.jsx
@@ -483,6 +483,7 @@ const TransactionCard = () => {
                             id="description"
                             name="description"
                             rows="3"
+                            required
                             defaultValue={data.description}
                             className="w-full p-3 rounded-md border border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500" />
                         </div>
